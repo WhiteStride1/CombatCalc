ce5bcc8748040fd4291d4b50a5387b8cb511058a
Jacob-Severson <jseverson@unomaha.edu>
2022-05-03 14:29:31 -0500

    started on button functionality

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ class GUI:
         self.strengthLabel = Label(window, text='Strength: ').grid(row=1, column=0, padx=5, pady=5)
         self.strengthEntry = Entry(window).grid(row=1, column=1, padx=5, pady=5)
 
-        self.strengthLabel = Label(window, text='Strength: ').grid(row=1, column=0, padx=5, pady=5)
-        self.strengthEntry = Entry(window).grid(row=1, column=1, padx=5, pady=5)
-
         self.defenseLabel = Label(window, text='Defense: ').grid(row=2, column=0, padx=5, pady=5)
         self.defenseEntry = Entry(window).grid(row=2, column=1, padx=5, pady=5)
 
@@ -28,7 +25,20 @@ class GUI:
         self.prayerLabel = Label(window, text='Prayer: ').grid(row=2, column=2, padx=5, pady=5)
         self.prayerEntry = Entry(window).grid(row=2, column=3, padx=5, pady=5)
 
-        self.calculateButton = Button(window, text='Calculate').grid(row=3, column=2, columnspan=2)
+        self.combat_level = Label(window, text="").grid(row=4, column=0)
+        self.error_label = Label(window, text="").grid(row=5, column=0)
+
+        self.calculateButton = Button(window, text='Calculate',command=self.clicked).grid(row=3, column=2, columnspan=2)
+    def clicked(self):
+        """
+        shows combat level in gui
+        :return:
+        """
+        if self.attackEntry.get()=="" or self.strengthEntry.get()=="" or self.defenseEntry.get() == "" or self.hitpointsEntry.get() == "" or self.rangeEntry.get() == "" or self.magicEntry.get() == "" or self.prayerEntry.get() == "":
+            self.error_label.config(text="please fill all entry")
+
+
+
 
 
 def main():
